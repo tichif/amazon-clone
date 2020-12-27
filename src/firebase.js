06346aab2e11a4ba12905de18fc82311602ca483
Dalzon Charles-Hébert <charlby5@gmail.com>
2020-12-26 23:41:05 -0600

    Set up the projet and create the header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCLZ_KsF5Np78RrKbwPz4eOGz0w5PtruWk',
+  authDomain: 'clone-80446.firebaseapp.com',
+  projectId: 'clone-80446',
+  storageBucket: 'clone-80446.appspot.com',
+  messagingSenderId: '102376365006',
+  appId: '1:102376365006:web:9b1c76ae4dbc43f971699f',
+  measurementId: 'G-DNK0NLVNKG',
+};
